b27d86be5ac3cc1fd4e44ded179a16cea8a29cf9
Dorota Jarecka <djarecka@gmail.com>
2022-11-17 14:45:35 -0500

    changing to test protocol

--- ui-changes/src/config.js
+++ ui-changes/src/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   /* eslint-disable */
-  githubSrc: 'https://raw.githubusercontent.com/ReproNim/demo-protocol/master/DemoProtocol/DemoProtocol_schema',
+  githubSrc: 'https://raw.githubusercontent.com/djarecka/test-protocol/master/DemoProtocol/DemoProtocol_schema',
   banner: 'This is a demonstration protocol for ReproSchema.',
   startButton: 'Join',
-  assetsPublicPath: '/demo-protocol/',
+  assetsPublicPath: '/test-protocol/',
   backendServer: null
 };
